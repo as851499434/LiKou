cc428badd2f1ce1834f43410d5fea51400f424df
as851499434 <liangbiyao000403>
2021-11-12 10:54:58 +0800

    修改

--- src/LiKou/DynamicProgram/DicesProbability.java
+++ src/LiKou/DynamicProgram/DicesProbability.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 /**
  * @Author: liangfan
  * @Date: 2021-11-11 16:28
- * @Description: 筛子点数概率 master
+ * @Description: 筛子点数概率 master1
  */
 public class DicesProbability {
     public double[] dicesProbability(int n) {
