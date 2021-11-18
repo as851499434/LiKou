c75d018d1b9ea45a57ad7d0c138c38b128663207
as851499434 <liangbiyao000403>
2021-11-18 14:37:54 +0800

    2021-11-18 双指针

---
+++ src/LiKou/DynamicProgram/RemoveDuplicates.java
@@ -0,0 +1,55 @@
+package LiKou.DynamicProgram;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+/**
+ * @Author: liangfan
+ * @Date: 2021-11-18 13:45
+ * @Description:
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * 输入：nums = [1,1,2]
+ * 输出：2, nums = [1,2]
+ * 解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+ */
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] ints = {-3,-1,0,0,0,3,3};
+        int i = removeDuplicates(ints);
+        System.out.println(i);
+    }
+    public static int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+
+
+
+//        int t = 0;
+//        for (int i = 0; i < nums.length; i ++ ) {
+//            if (i == 0 || nums[i] != nums[i - 1]) {
+//                nums[t ++ ] = nums[i];
+//            }
+//        }
+//        System.out.println(Arrays.toString(nums));
+//        return t;
+    }
+}
--- src/LiKou/Main.java
+++ src/LiKou/Main.java
@@ -1,5 +1,8 @@
 package LiKou;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 /**
  * @Author: liangfan
  * @Date: 2021-11-12 11:20
@@ -7,6 +10,59 @@ package LiKou;
  */
 public class Main {
     public static void main(String[] args) {
+        List<Person> list = new ArrayList<Person>();
+        Person p1 = new Person("a", 11, "aaaaa");
+        Person p2 = new Person("b", 12, "bbbbb");
+        Person p3 = new Person("c", 13, "ccccc");
+        Person p4 = new Person("d", 14, "ddddd");
+        Person p5 = new Person("e", 15, "eeeee");
+        list = Arrays.asList(p1, p2, p3, p4, p5);
+        List<Person> a = list.stream().sorted(Comparator.comparingInt(Person::getAge)).collect(Collectors.toList());
+        int asInt = list.stream().mapToInt(Person::getAge).max().getAsInt();
+        list.stream().mapToInt(Person::getAge).reduce(0, Integer::sum);
+        System.out.println();
+        System.out.println(asInt);
+        System.out.println(a);
+    }
+}
 
+class Person {
+
+    private String name;
+    private int age;
+    private String province;
+
+    public Person(String name, int age, String province) {
+        this.name = name;
+        this.age = age;
+        this.province = province;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getProvince() {
+        return province;
+    }
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", province='" + province + '\'' +
+                '}';
     }
 }
