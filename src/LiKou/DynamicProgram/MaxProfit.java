82274e0bd03ae4a9158b1c034adba121c8b4d0b8
as851499434 <liangbiyao000403>
2021-12-22 14:39:14 +0800

    2021-12-22 数组 动态规划

--- src/LiKou/DynamicProgram/MaxProfit.java
+++ src/LiKou/DynamicProgram/MaxProfit.java
@@ -8,18 +8,19 @@ package LiKou.DynamicProgram;
  * 输入: [7,1,5,3,6,4]
  * 输出: 5
  * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
- *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
  * 输入: [7,6,4,3,1]
  * 输出: 0
  * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
  */
 public class MaxProfit {
     public static void main(String[] args) {
-//        int[] ints = {7,1,5,3,6,4};
-        int[] ints = {7,6,4,3,1};
+        int[] ints = {7, 1, 5, 3, 6, 4};
+//        int[] ints = {7,6,4,3,1};
         int i = maxProfit(ints);
         System.out.println(i);
     }
+
     public static int maxProfit(int[] prices) {
         int cost = Integer.MAX_VALUE, profit = 0;
         for (int price : prices) {
