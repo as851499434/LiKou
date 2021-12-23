fa77892c996e2e89557a6c0381fa6d3251b5db4f
as851499434 <liangbiyao000403>
2021-12-23 15:32:38 +0800

    2021-12-23 给庄陶泽的题目 我觉得很经典

---
+++ src/TDR/ClimbStairs.java
@@ -0,0 +1,22 @@
+package TDR;
+
+/**
+ * @Author: liangfan
+ * @Date: 2021-12-21 14:49
+ * @Description: 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ * 注意：给定 n 是一个正整数。
+ */
+
+public class ClimbStairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(44));
+    }
+
+    public static int climbStairs(int n) {
+        return 0;
+    }
+
+
+}
