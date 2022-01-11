62f43ccb0bd8b84f38e5e98e7bce926af0aab7d8
as851499434 <liangbiyao000403>
2022-01-11 13:28:25 +0800

    2022-01-11 简单理解题

---
+++ src/LfLiKou/EveryDay/MaxDepth.java
@@ -0,0 +1,48 @@
+package LfLiKou.EveryDay;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @Author: liangfan
+ * @Date: 2022-01-07 09:58
+ * @Description: 括号的最大嵌套深度
+ *
+ * 如果字符串满足以下条件之一，则可以称之为 有效括号字符串（valid parentheses string，可以简写为 VPS）：
+ *
+ * 字符串是一个空字符串 ""，或者是一个不为 "(" 或 ")" 的单字符。
+ * 字符串可以写为 AB（A 与 B字符串连接），其中 A 和 B 都是 有效括号字符串 。
+ * 字符串可以写为 (A)，其中 A 是一个 有效括号字符串 。
+ * 类似地，可以定义任何有效括号字符串?S 的 嵌套深度 depth(S)：
+ *
+ * depth("") = 0
+ * depth(C) = 0，其中 C 是单个字符的字符串，且该字符不是 "(" 或者 ")"
+ * depth(A + B) = max(depth(A), depth(B))，其中 A 和 B 都是 有效括号字符串
+ * depth("(" + A + ")") = 1 + depth(A)，其中 A 是一个 有效括号字符串
+ * 例如：""、"()()"、"()(()())" 都是 有效括号字符串（嵌套深度分别为 0、1、2），而 ")(" 、"(()" 都不是 有效括号字符串 。
+ *
+ * 给你一个 有效括号字符串 s，返回该字符串的 s 嵌套深度 。
+ *
+ * 输入：s = "(1+(2*3)+((8)/4))+1"
+ * 输出：3
+ * 输入：s = "(1)+((2))+(((3)))"
+ * 输出：3
+ */
+
+public class MaxDepth {
+    public static void main(String[] args) {
+
+    }
+    // 根据题意，其实就是求最大的连续左括号的数量（跳过普通字符，且与 ) 抵消后），只需要边遍历边统计即可。
+    public int maxDepth(String s) {
+        int n = s.length(), ans = 0;
+        for (int i = 0, cnt = 0; i < n; i++) {
+            if (s.charAt(i) == '(') cnt++;
+            else if (s.charAt(i) == ')') cnt--;
+            ans = Math.max(ans, cnt);
+        }
+        return ans;
+    }
+
+}
